7a8e365de0125c09609e7dcc3e75e8d223936f5c
Wen <wenrg@users.noreply.github.com>
2016-11-17 14:48:04 -0500

    those two continuous vars

--- 08ClusterAnalysisHomework.R
+++ 08ClusterAnalysisHomework.R
@@ -1,4 +1,3 @@
 ############################################################################ - @wen
-plot(allstate.train[, 118], allstate.train[, 130]) # cont117 and cont129
-
+plot(allstate.train[, 118], allstate.train[, 130]) # cont117 and cont129, aes(fill = cat_), will find out the key categorical var.
 
